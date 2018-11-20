8dd0c722f79c61934f259d85a63c0154d0c1845b
openx0125 <35224336+openx0125@users.noreply.github.com>
2018-11-20 10:40:38 +0800

    Add files via upload

---
+++ labs/solutions/exercise22/js/mylib.js
@@ -0,0 +1,160 @@
+function id(idName) {
+	return document.getElementById(idName);
+}
+
+function proxy(dom, handler) {
+    return function() {
+        return handler.apply(dom, arguments);
+    };
+}
+
+function bind(dom, eventName, handler) {
+	if(dom.addEventListener) {
+	    dom.addEventListener(eventName, handler, false);
+	} else if(dom.attachEvent) {
+		var hd = proxy(dom, handler);
+		
+		dom.attachEvent('on' + eventName, function() {
+			var event = {
+			    'currentTarget' : dom,
+				'target' : window.event.srcElement
+		    };
+			hd(event);
+		});
+		
+	}
+}
+
+function style(obj, prop) {
+	if(window.getComputedStyle) {
+		return window.getComputedStyle(obj, null)[prop];
+	}
+	else if(obj.currentStyle) {
+		return obj.currentStyle[prop];
+	}
+	else {
+		return null;
+	}
+}
+
+function show(element) {
+	element.style.display = element.previousDisplay || '';
+	if(style(element, 'display') === 'none') {
+		var node = document.createElement(element.nodeName);
+		document.body.appendChild(node);
+		element.style.display = style(node, 'display');
+		document.body.removeChild(node);
+	}
+}
+
+function hide(element) {
+	element.previousDisplay = style(element, 'display');
+	element.style.display = 'none';
+}
+
+function opacity(element, value) {
+	if(value === undefined) { // 取得不透明度
+		var opt = style(element, 'opacity') || style(element, 'filter');
+		if(opt === '') {
+			return 1;
+		}
+		if(opt.indexOf('alpha') !== -1)  {
+			return opt.substring(14, opt.length - 1) / 100;
+		}
+		return parseFloat(opt);
+	}
+
+	if(style(element, 'opacity') !== undefined) {
+		element.style.opacity = value;
+	}
+	else {
+		element.style.filter = 
+		   'alpha(opacity=' + parseInt(value * 100) + ')';
+	}
+}
+
+function fadeIn(element, speed, steps) {
+    speed = speed || 5000;
+    steps = steps || 10;
+    var target = element.previousOpacity || 1;
+    delete element.previousOpacity;
+ 
+    var timeInterval = speed / steps;
+    var valueStep = target / steps;
+
+    var opt = 0;
+    setTimeout(function next() {
+        opt += valueStep;
+        if(opt < target) {
+            opacity(element, opt);
+            setTimeout(next, timeInterval);
+        }
+        else {
+            opacity(element, target);
+        }
+    }, timeInterval);
+}                
+
+function fadeOut(element, speed, steps) {
+    speed = speed || 5000;
+    steps = steps || 10;
+    element.previousOpacity = opacity(element);
+    
+    var timeInterval = speed / steps;
+    var valueStep = element.previousOpacity / steps;
+
+    var opt = element.previousOpacity;
+    setTimeout(function next() {
+        opt -= valueStep;
+        if(opt > 0) {
+            opacity(element, opt);
+            setTimeout(next, timeInterval);
+        }
+        else {
+            opacity(element, 0);
+        }
+    }, timeInterval);
+}
+
+function fadeIn(element, speed, steps) {
+    speed = speed || 5000;
+    steps = steps || 10;
+    var target = element.previousOpacity || 1;
+    delete element.previousOpacity;
+ 
+    var timeInterval = speed / steps;
+    var valueStep = target / steps;
+
+    var opt = 0;
+    setTimeout(function next() {
+        opt += valueStep;
+        if(opt < target) {
+            opacity(element, opt);
+            setTimeout(next, timeInterval);
+        }
+        else {
+            opacity(element, target);
+        }
+    }, timeInterval);
+}                
+
+function fadeOut(element, speed, steps) {
+    speed = speed || 5000;
+    steps = steps || 10;
+    element.previousOpacity = opacity(element);
+    
+    var timeInterval = speed / steps;
+    var valueStep = element.previousOpacity / steps;
+
+    var opt = element.previousOpacity;
+    setTimeout(function next() {
+        opt -= valueStep;
+        if(opt > 0) {
+            opacity(element, opt);
+            setTimeout(next, timeInterval);
+        }
+        else {
+            opacity(element, 0);
+        }
+    }, timeInterval);
+}
